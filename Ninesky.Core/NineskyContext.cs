4a35da09d3f21606addfc391d42dcc5e654ecbeb
徐小平 <2533566560@qq.com>
2018-09-25 20:03:20 +0800

    改1

---
+++ Ninesky.Core/NineskyContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Ninesky.Core
+{
+    /// <summary>
+    /// 数据上下文
+    /// </summary>
+    public class NineskyContext : DbContext
+    {
+        public NineskyContext() : base("DefaultConnection")
+        {
+            Database.SetInitializer<NineskyContext>(new CreateDatabaseIfNotExists<NineskyContext>());
+        }
+    }
+}
