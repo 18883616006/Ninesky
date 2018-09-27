fa8a90a3a095bf1a54ff95db91dadf1fa4e4cc91
徐小平 <2533566560@qq.com>
2018-09-27 19:44:36 +0800

    改3

--- Ninesky.Core/NineskyContext.cs
+++ Ninesky.Core/NineskyContext.cs
@@ -16,6 +16,7 @@ namespace Ninesky.Core
         /// 管理员集合
         /// </summary>
         public DbSet<Administrator> Administrators { get; set; }
+        public DbSet<Role> Roles { get; set; }
 
         public NineskyContext() : base("DefaultConnection")
         {
